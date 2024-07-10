80c6fe018a5437469d67ddf2fa8be067950597d5
Somia Farag <somiafarag5@gmail.com>
2024-07-10 02:38:57 +0000

    lab completed

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,8 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        return flightDAO.insertFlight(flight);
+
     }
 
     /**
@@ -70,6 +71,14 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
+        // Check if the flight with the given ID exists
+        Flight existingFlight = flightDAO.getFlightById(flight_id);
+        if (existingFlight != null) {
+            // Update the flight if it exists
+            flightDAO.updateFlight(flight_id, flight);
+            return flightDAO.getFlightById(flight_id); 
+            // Return the updated flight
+        }
         return null;
     }
 
@@ -80,7 +89,7 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        return flightDAO.getAllFlights();
     }
 
     /**
@@ -92,6 +101,6 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+        return flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
     }
 }
